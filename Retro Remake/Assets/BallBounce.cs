4c4be4452b91d708771f782a1d0ce655ae88922b
MUKILTEO\1721537 <1721537@mukilteo.wednet.edu>
2024-09-17 10:21:18 -0700

    Created the start for the second level stage, programmed the movement of the platform and made sure it only moves left and right and does stop, started programming of the balls movement and bounce.

---
+++ Retro Remake/Assets/BallBounce.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBounce : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = Vector2.up;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Retro Remake/Assets/Movement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public KeyCode left = KeyCode.A, right = KeyCode.D;
+    public float speed = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(left))
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector2.left * speed;
+        }
+
+        if (Input.GetKey(right))
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+        }
+    }
+}
